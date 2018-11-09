4ebddc275d49fb59c7c6ca4b9e4265989e0d22bd
Dentioner <we980127@gmail.com>
2018-11-09 23:16:25 +0800

    until p70

--- add16/add16.sim/sim_1/behav/xsim/xsim.dir/test_behav/webtalk/xsim_webtalk.tcl
+++ add16/add16.sim/sim_1/behav/xsim/xsim.dir/test_behav/webtalk/xsim_webtalk.tcl
@@ -1,6 +1,6 @@
 webtalk_init -webtalk_dir E:/coding/verilog/Verilog/add16/add16.sim/sim_1/behav/xsim/xsim.dir/test_behav/webtalk/
 webtalk_register_client -client project
-webtalk_add_data -client project -key date_generated -value "Tue Oct 30 11:35:11 2018" -context "software_version_and_target_device"
+webtalk_add_data -client project -key date_generated -value "Fri Nov  9 23:12:37 2018" -context "software_version_and_target_device"
 webtalk_add_data -client project -key product_version -value "XSIM v2018.2 (64-bit)" -context "software_version_and_target_device"
 webtalk_add_data -client project -key build_version -value "2258646" -context "software_version_and_target_device"
 webtalk_add_data -client project -key os_platform -value "WIN64" -context "software_version_and_target_device"
@@ -14,7 +14,7 @@ webtalk_add_data -client project -key target_package -value "not_applicable" -co
 webtalk_add_data -client project -key target_speed -value "not_applicable" -context "software_version_and_target_device"
 webtalk_add_data -client project -key random_id -value "c47c2b4b-da4f-442d-ba48-1f8d336733a4" -context "software_version_and_target_device"
 webtalk_add_data -client project -key project_id -value "8df75595f772414eb3371afc2871381e" -context "software_version_and_target_device"
-webtalk_add_data -client project -key project_iteration -value "31" -context "software_version_and_target_device"
+webtalk_add_data -client project -key project_iteration -value "33" -context "software_version_and_target_device"
 webtalk_add_data -client project -key os_name -value "Microsoft Windows 7 , 64-bit" -context "user_environment"
 webtalk_add_data -client project -key os_release -value "Service Pack 1  (build 7601)" -context "user_environment"
 webtalk_add_data -client project -key cpu_name -value "Intel(R) Core(TM) i5-6200U CPU @ 2.30GHz" -context "user_environment"
@@ -22,12 +22,11 @@ webtalk_add_data -client project -key cpu_speed -value "2400 MHz" -context "user
 webtalk_add_data -client project -key total_processors -value "1" -context "user_environment"
 webtalk_add_data -client project -key system_ram -value "12.000 GB" -context "user_environment"
 webtalk_register_client -client xsim
-webtalk_add_data -client xsim -key runall -value "true" -context "xsim\\command_line_options"
 webtalk_add_data -client xsim -key Command -value "xsim" -context "xsim\\command_line_options"
 webtalk_add_data -client xsim -key trace_waveform -value "true" -context "xsim\\usage"
-webtalk_add_data -client xsim -key runtime -value "60865110 ns" -context "xsim\\usage"
-webtalk_add_data -client xsim -key iteration -value "0" -context "xsim\\usage"
-webtalk_add_data -client xsim -key Simulation_Time -value "47.66_sec" -context "xsim\\usage"
-webtalk_add_data -client xsim -key Simulation_Memory -value "16744_KB" -context "xsim\\usage"
-webtalk_transmit -clientid 1059094428 -regid "" -xml E:/coding/verilog/Verilog/add16/add16.sim/sim_1/behav/xsim/xsim.dir/test_behav/webtalk/usage_statistics_ext_xsim.xml -html E:/coding/verilog/Verilog/add16/add16.sim/sim_1/behav/xsim/xsim.dir/test_behav/webtalk/usage_statistics_ext_xsim.html -wdm E:/coding/verilog/Verilog/add16/add16.sim/sim_1/behav/xsim/xsim.dir/test_behav/webtalk/usage_statistics_ext_xsim.wdm -intro "<H3>XSIM Usage Report</H3><BR>"
+webtalk_add_data -client xsim -key runtime -value "1 us" -context "xsim\\usage"
+webtalk_add_data -client xsim -key iteration -value "1" -context "xsim\\usage"
+webtalk_add_data -client xsim -key Simulation_Time -value "0.09_sec" -context "xsim\\usage"
+webtalk_add_data -client xsim -key Simulation_Memory -value "8724_KB" -context "xsim\\usage"
+webtalk_transmit -clientid 413370591 -regid "" -xml E:/coding/verilog/Verilog/add16/add16.sim/sim_1/behav/xsim/xsim.dir/test_behav/webtalk/usage_statistics_ext_xsim.xml -html E:/coding/verilog/Verilog/add16/add16.sim/sim_1/behav/xsim/xsim.dir/test_behav/webtalk/usage_statistics_ext_xsim.html -wdm E:/coding/verilog/Verilog/add16/add16.sim/sim_1/behav/xsim/xsim.dir/test_behav/webtalk/usage_statistics_ext_xsim.wdm -intro "<H3>XSIM Usage Report</H3><BR>"
 webtalk_terminate
